8043fb4d569b8d5117b62e7325908118d6160f74
Jay Korat <koratjay03@gmail.com>
2023-11-09 13:11:54 +0530

    till lec 30

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -1,12 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+
 contract Faucet {
   
+  // this is a special function
+  // it's called when you make a tx that doesn't specify
+  // function name to call
+
+  // External function are part of the contract interface
+  // which means they can be called via contracts and other txs
 
   receive() external payable{}
+
+
   
 
-
 }
+// Block info
+// Nonce - a hash that when combined with the minHash proofs that
+// the block has gone through proof of work(POW)
+// 8 bytes => 64 bits
 
 // const instance = await Faucet.deployed()
