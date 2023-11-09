7b04fb2cdc4db84d396f473327169b7deed68a68
Jay Korat <koratjay03@gmail.com>
2023-11-09 15:02:06 +0530

    till lec 50

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -6,30 +6,35 @@ contract Faucet {
   // internal -> can be accesible within smart contract and also derived smart contract
 
     uint public numOfFunders;
-  mapping(uint => address) private funders;
+  mapping(address => bool) private funders;
+  mapping(uint =>address) private lutfunders;
 
   receive() external payable{}
 
   function addFunds() external payable{
-    uint index = numOfFunders++;
-    funders[index] = msg.sender;
+    address funder = msg.sender;
+      if (!funders[funder]) {
+      uint index = numOfFunders++;
+      funders[funder] = true;
+      lutfunders[index] = funder;
+    }
   }
-
+  
   function getAllFunders() external view returns (address[] memory) {
     address[] memory _funders = new address[](numOfFunders);
 
     for (uint i = 0; i < numOfFunders; i++) {
-      _funders[i] = funders[i];
+      _funders[i] = lutfunders[i];
     }
 
     return _funders;
   }
  
-
+  
   function getFunderAtIndex(uint8 index) external view returns(address){
     // address[] memory _funders = this.getallFunders();  u can use external and call the function like this but it will use more gas
     
-    return funders[index];
+    return lutfunders[index];
   }
   
 
@@ -40,7 +45,7 @@ contract Faucet {
 // instance.addFunds({from: accounts[0], value: "200000000"})
 // instance.addFunds({from: accounts[1], value: "200000000"})
 // instance.getFunderAtIndex(0)
-//nstance.getAllFunders() i
+// instance.getAllFunders()
 
 
 // Block info
