ca64f9610887558cdbd1dfaac6cff2b3b9116b45
Jay Korat <koratjay03@gmail.com>
2023-11-09 16:00:41 +0530

    till lec 60

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,8 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 import "./Owned.sol";
 import "./Logger.sol";
+import "./IFaucet.sol";
 
-contract Faucet is Owned, Logger {
+contract Faucet is Owned, Logger , IFaucet {
   // private -> can be accesible only within the smart contract
   // internal -> can be accesible within smart contract and also derived smart contract
 
@@ -27,7 +28,7 @@ contract Faucet is Owned, Logger {
     return "Hello World";
   }
 
-  function addFunds() external payable{
+  function addFunds() override external payable{
     address funder = msg.sender;
       if (!funders[funder]) {
       uint index = numOfFunders++;
@@ -44,7 +45,7 @@ contract Faucet is Owned, Logger {
     // some managing stuff that only admin should have access to
   }
 
-    function withdraw(uint withdrawAmount) external limitWithdraw(withdrawAmount) {
+    function withdraw(uint withdrawAmount) override external limitWithdraw(withdrawAmount) {
     payable(msg.sender).transfer(withdrawAmount);
   }
   
@@ -72,7 +73,7 @@ contract Faucet is Owned, Logger {
 // const instance = await Faucet.deployed();
 // instance.addFunds({from: accounts[0], value: "2000000000000000000"})
 // instance.addFunds({from: accounts[1], value: "2000000000000000000"})
-// instance.withdraw("500000000000000000", {from: accounts[1]})
+// instance.withdraw("50000000000000000", {from: accounts[1]})
 // instance.getFunderAtIndex(0)
 // instance.getAllFunders()
 
