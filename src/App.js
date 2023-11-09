3e0f23db2d0675d9d320e7bbd5678a534900aa4a
Jay Korat <koratjay03@gmail.com>
2023-11-09 16:15:30 +0530

    till lec 67

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <div className="faucet-wrapper">
+        <div className="faucet">
+          <div className="balance-view is-size-2">
+            Current Balance: <strong>10</strong> ETH
+          </div>
+          <button className="btn mr-2">Donate</button>
+          <button className="btn">Withdraw</button>
+        </div>
+      </div>
+    </>
   );
 }
 
